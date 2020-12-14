fa762d881b2229395cb74bfcee33f34ef8f36702
Maciej Kruszewski <m.kruszewski@sunflux.pl>
2020-12-14 20:58:03 +0100

    #14 fix :)

--- pages/api/v1/products.ts
+++ pages/api/v1/products.ts
@@ -1,9 +1,8 @@
 import {NextApiRequest, NextApiResponse} from "next";
-// import ProductsRepository from "@repository/products.repository";
-import {PaypalRepository} from "@repository/paypal.repository";
+import ProductsRepository from "@repository/products.repository";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    // const productsRepository: ProductsRepository = await ProductsRepository.build();
+    const productsRepository: ProductsRepository = await ProductsRepository.build();
 
-    res.status(200).json(await (await PaypalRepository.build()).getPayment('PAYID-L7L26GQ34E773272A787224P'))
+    res.status(200).json(await productsRepository.findAll())
 }
