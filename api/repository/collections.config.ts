6f72c7ba2049bdaee450833861f98bb0cab3b661
Maciej Kruszewski <m.kruszewski@sunflux.pl>
2021-01-06 18:06:36 +0100

    #23 shipping methods

--- api/repository/collections.config.ts
+++ api/repository/collections.config.ts
@@ -2,3 +2,4 @@ export const PC_CONFIGURATION_COLLECTION = 'pc_configurations';
 export const ORDER_COLLECTION = 'orders';
 export const PRODUCTS_COLLECTION = 'products';
 export const NEWS_COLLECTION = 'news';
+export const SHIPPING_METHODS_COLLECTION = 'shipping_methods';
