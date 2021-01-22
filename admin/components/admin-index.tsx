81437200f272428debf989f9bf5a522e0bce6fbf
Maciej Kruszewski <m.kruszewski@sunflux.pl>
2021-01-22 23:37:40 +0100

    authorization

--- admin/components/admin-index.tsx
+++ admin/components/admin-index.tsx
@@ -2,12 +2,59 @@ import * as React from "react";
 import {Admin, ListGuesser, Resource} from 'react-admin';
 import {createHashHistory} from 'history';
 import simpleRestProvider from 'ra-data-simple-rest';
+import { AuthProvider } from 'react-admin';
+
+const authProvider: AuthProvider = {
+    checkAuth(params: any): Promise<void> {
+        const promise: Promise<void> = new Promise((resolve, reject) => {
+            const auth = localStorage.getItem('auth');
+
+            if (auth === '' || auth === undefined || auth === null) {
+                reject()
+            }
+
+            resolve()
+        })
+
+        return promise;
+    }, checkError(error: any): Promise<void> {
+        return Promise.resolve(undefined);
+    }, getPermissions(params: any): Promise<any> {
+        return Promise.resolve(undefined);
+    }, login({ username, password }): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const request = new Request(`${process.env.API_HOST}/api/v1/admin/auth`, {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: new Headers({ 'Content-Type': 'application/json' }),
+            });
+
+            fetch(request)
+                .then(response => {
+                    if (response.status < 200 || response.status >= 300) {
+                        reject();
+                    }
+                    localStorage.setItem('auth', JSON.stringify(response.json()));
+
+                    resolve();
+                })
+                .catch(() => {
+                    reject();
+                });
+        });
+    }, logout(params: any): Promise<void | false | string> {
+        localStorage.removeItem('auth');
+
+        return Promise.resolve('login');
+    }
+}
 
 const history = createHashHistory();
 
-const App = () => <Admin dataProvider={simpleRestProvider(`${process.env.API_HOST}/api/v1/admin`)} history={history}>
+const App = () => <Admin authProvider={authProvider} dataProvider={simpleRestProvider(`${process.env.API_HOST}/api/v1/admin`)} history={history}>
     <Resource name="news" list={ListGuesser} />
     <Resource name="orders" list={ListGuesser} />
+    {/*<Resource name="carts" list={ListGuesser} />*/}
     <Resource name="products" list={ListGuesser} />
     <Resource name="configurations" list={ListGuesser} />
     <Resource name="shipping-methods" list={ListGuesser} />
---
+++ api/pages/api/v1/admin/auth.ts
@@ -0,0 +1,24 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import NewsRepository from "@apiRepository/news.repository";
+import {getPagination} from "@apiMiddleware/pagination.middleware";
+import {mapMongoId} from "@apiMiddleware/mongo.middleware";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'OPTIONS') return res.status(200).json({});
+
+    if (req.method === 'POST') {
+        await post(req, res);
+    } else {
+        res.status(405).json({code: 'not-supported'});
+    }
+}
+
+const post = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.body.username !== 'admin' || req.body.password !== 'admin' ) {
+        res.status(401).json({isAuthorized: false});
+
+        return;
+    }
+
+    res.status(200).json({isAuthorized: true})
+};
